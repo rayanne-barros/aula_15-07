1ccb834ee8cf852684ffad2a21ac8bd7036858b2
rayanne-barros <rayanneb126@gmail.com>
2023-07-15 14:43:17 -0300

    Classe Python

--- manipulacao_json.py
+++ manipulacao_json.py
@@ -1,8 +1,12 @@
 import json
 
 json_conta = '{"id": 1, "nome": "Rayanne", "saldo": 2000}'
+
+# convertendo para dicionário
 conta = json.loads(json_conta)
 
 print(conta["id"])
 print(conta["nome"])
 print(conta["saldo"])
+
+# 3 operações essenciais: entrada, processamento e saída
