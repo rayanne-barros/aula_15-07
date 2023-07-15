d6c89738a9ac8355638b027db0c4b54004ac904e
rayanne-barros <rayanneb126@gmail.com>
2023-07-15 15:46:12 -0300

    Add resolução do professor

--- classe.py
+++ classe.py
@@ -1,10 +1,51 @@
-class Pessoa:
-    def __init__(self, nome, idade):
+class Personagem:
+    def __init__(self, nome, pontuacao, moedas, poder_especial):
         self.nome = nome
-        self.idade = idade
+        self.idade = pontuacao
+        self.moedas = moedas
+        self.poder_especial = poder_especial
 
-    def saudacao(self):
-        print(f'Olá, meu nome é {self.nome} e eu tenho {self.idade} anos')
+    def usar_poder(self):
+        if (self.nome == "Mario"):
+            return print(f'{self.nome}: {self.poder_especial}!')
+        elif (self.nome == "Luigi"):
+            return print(f"{self.nome}: {self.poder_especial}!")
+        else:
+            return print(f'{self.nome}: {self.poder_especial}')
 
-pessoa1 = Pessoa("Rayanne", 28)
-pessoa1.saudacao()
+
+personagem1 = Personagem("Mario", 20, 300, "Super Mario")
+personagem1.usar_poder()
+personagem2 = Personagem("Luigi", 15, 200, "Fire Luigi")
+personagem2.usar_poder()
+personagem3 = Personagem("Yoshi", 10, 100, "Flying Yoshi")
+personagem3.usar_poder()
+
+
+#### Resolução Professor ####
+
+class Personagem:
+    def __init__(self, nome, pontuacao, moedas):
+        self.nome = nome
+        self.idade = pontuacao
+        self.moedas = moedas
+
+    def usar_poder(self):
+        if (self.nome == "Mario"):
+            return "Super Mario"
+
+        elif (self.nome == "Luigi"):
+            return "Fire Luigi"
+
+        else:
+            return "Flying Yoshi"
+
+
+personagem1 = Personagem("Mario", 20, 300)
+print(f"Nome do Personagem: {personagem1.nome} Poder Especial: {personagem1.usar_poder()}!")
+personagem2 = Personagem("Luigi", 15, 200)
+print(f"Nome do Personagem: {personagem2.nome} Poder Especial: {personagem2.usar_poder()}!")
+personagem3 = Personagem("Yoshi", 10, 100)
+print(f"Nome do Personagem: {personagem3.nome} Poder Especial: {personagem3.usar_poder()}!")
+
+
